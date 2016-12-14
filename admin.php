364592ff8c3a5ed7d24f605920872a4427c49d56
plegall <plg@piwigo.org>
2016-12-14 15:01:05 +0100

    Check file sums against what they should be.
    Include Piwigo versions 2.7.x and 2.8.x

---
+++ admin.php
@@ -0,0 +1,87 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
+if( !defined("PHPWG_ROOT_PATH") )
+{
+  die ("Hacking attempt!");
+}
+
+// +-----------------------------------------------------------------------+
+// | Check Access and exit when user status is not ok                      |
+// +-----------------------------------------------------------------------+
+
+check_status(ACCESS_WEBMASTER);
+
+// +-----------------------------------------------------------------------+
+// |                            add permissions                            |
+// +-----------------------------------------------------------------------+
+
+if (isset($_POST['submit']))
+{
+  $file_sums = dirname(__FILE__).'/data/piwigo-'.PHPWG_VERSION.'-sums.txt';
+  
+  $lines = file($file_sums);
+
+  foreach ($lines as $line)
+  {
+    list($sum_ref, $relative_path) = explode(' ', trim($line));
+
+    $fullpath = './'.$relative_path;
+
+    if (!is_file($fullpath))
+    {
+      array_push($page['errors'], $relative_path.' is missing');
+      continue;
+    }
+
+    $sum_local = sha1(file_get_contents($fullpath));
+
+    if ($sum_local != $sum_ref)
+    {
+      array_push($page['errors'], $relative_path.' has been modified');
+    }
+  }
+
+  if (count($page['errors']) == 0)
+  {
+    array_push($page['infos'], 'Well done! Everything seems good :-)');
+  }
+}
+
+
+// +-----------------------------------------------------------------------+
+// |                             template init                             |
+// +-----------------------------------------------------------------------+
+
+$template->set_filenames(
+  array(
+    'plugin_admin_content' => dirname(__FILE__).'/admin.tpl'
+    )
+  );
+
+// +-----------------------------------------------------------------------+
+// |                           sending html code                           |
+// +-----------------------------------------------------------------------+
+
+$template->assign_var_from_handle('ADMIN_CONTENT', 'plugin_admin_content');
+?>
---
+++ admin.tpl
@@ -0,0 +1,11 @@
+<div class="titrePage">
+  <h2>Check Files Integrity</h2>
+</div>
+
+<form method="post" action="">
+  <p>
+    <button name="submit" type="submit" class="buttonLike">
+      <i class="icon-cog-alt"></i> {'Check Piwigo files'|@translate}
+    </button>
+  </p>
+</form>
