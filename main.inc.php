a471ab1ed271a4b00e19956db55b4131f72908fd
plegall <plg@piwigo.org>
2021-10-13 14:10:50 +0200

    compatibility with Piwigo 12

--- main.inc.php
+++ main.inc.php
@@ -6,25 +6,11 @@ Description: Check Piwigo files for unexpected modifications
 Plugin URI: http://piwigo.org/ext/extension_view.php?eid=
 Author: plg
 Author URI: http://piwigo.org
+Has Settings: true
 */
 
 if (!defined('PHPWG_ROOT_PATH'))
 {
   die('Hacking attempt!');
-}
-
-/* Plugin admin */
-add_event_handler('get_admin_plugin_menu_links', 'cfi_admin_menu');
-function cfi_admin_menu($menu)
-{
-  array_push(
-    $menu,
-    array(
-      'NAME' => 'Check Files Integrity',
-      'URL'  => get_root_url().'admin.php?page=plugin-check_files_integrity',
-      )
-    );
-
-  return $menu;
 }
 ?>
